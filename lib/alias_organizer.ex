385c592482cd426eaa50c6f60e7380632dd88110
Paul Fleischer <paul@xpg.dk>
2021-12-05 01:28:53 +0100

    Insert an additional whitespace between alias expressions and any following non-alias expression

--- lib/alias_organizer.ex
+++ lib/alias_organizer.ex
@@ -8,13 +8,29 @@ defmodule AliasOrganizer do
   def process_file(file) when is_binary(file) do
     IO.puts("Processing #{file}")
 
-    content =
+    input_ast =
       File.read!(file)
       |> Sourceror.parse_string!()
-      |> process_file_ast()
-      |> Sourceror.to_string()
 
-    File.write!(file, content)
+    output_ast =
+      input_ast
+      |> process_file_ast()
+
+    if input_ast != output_ast do
+      content =
+        Sourceror.to_string(output_ast)
+        |> insert_newline_between_alias_and_other_content()
+
+      File.write!(file, content)
+    end
+  end
+
+  @alias_without_two_newlines_pattern ~R/(alias.*[^{]\n)((?![ ]*(?:alias|\n) .*).*)/
+
+  defp insert_newline_between_alias_and_other_content(content) do
+    Regex.scan(@alias_without_two_newlines_pattern, content)
+    |> IO.inspect
+    Regex.replace(@alias_without_two_newlines_pattern, content, "\\1\n\\2")
   end
 
   defp process_file_ast({:__block__, meta, block}) do
@@ -40,22 +56,38 @@ defmodule AliasOrganizer do
   def process_module({:defmodule, meta, [alias_name | [body]]}) do
     module_name = Alias.alias_to_string(alias_name)
 
-    IO.puts("Processing '#{module_name}'")
+    if skip_module?(meta) do
+      IO.puts("Skipping '#{module_name}'")
 
-    aliased_modules = Collector.collect_aliased_modules(body)
+      body =
+        body
+        |> process_file_ast()
 
-    body_without_alias_expressions = Mutator.strip_alias_expressions(body)
+      {:defmodule, meta, [alias_name | [body]]}
+    else
+      IO.puts("Processing '#{module_name}'")
 
-    used_aliases = Collector.collect_used_aliases(body_without_alias_expressions, aliased_modules)
+      aliased_modules = Collector.collect_aliased_modules(body)
 
-    short_version_map = Shortener.determine_short_versions(used_aliases)
+      body_without_alias_expressions = Mutator.strip_alias_expressions(body)
 
-    body =
-      body_without_alias_expressions
-      |> Mutator.replace_aliases(short_version_map, aliased_modules)
-      |> Mutator.add_alias_expressions(short_version_map)
-      |> process_file_ast()
+      used_aliases =
+        Collector.collect_used_aliases(body_without_alias_expressions, aliased_modules)
 
-    {:defmodule, meta, [alias_name | [body]]}
+      short_version_map = Shortener.determine_short_versions(used_aliases)
+
+      body =
+        body_without_alias_expressions
+        |> Mutator.replace_aliases(short_version_map, aliased_modules)
+        |> Mutator.add_alias_expressions(short_version_map)
+        |> process_file_ast()
+
+      {:defmodule, meta, [alias_name | [body]]}
+    end
+  end
+
+  defp skip_module?(meta) do
+    leading_comments = Keyword.get(meta, :leading_comments, [])
+    Map.get(List.last(leading_comments) || %{}, :text) == "# @NoOrganizeAliases"
   end
 end
